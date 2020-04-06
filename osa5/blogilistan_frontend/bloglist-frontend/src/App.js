2088c7ceb4b3dee0b620e4d540d12ee1a7b2c189
aantikainen <arttu.antikainen@outlook.com>
2020-04-06 22:31:05 +0300

    ex1 part5

--- osa5/blogilistan_frontend/bloglist-frontend/src/App.js
+++ osa5/blogilistan_frontend/bloglist-frontend/src/App.js
@@ -2,10 +2,6 @@ import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
-// import blogForm from './components/blogForm'
-// import loginForm from './components/loginForm'
-
-
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
